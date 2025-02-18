1d6e5130f1f3893af80aab0cdc74cbc883dcd542
Luisa <mlumeca@gmail.com>
2025-02-18 22:28:43 +0100

    proyecto creado

---
+++ src/main/java/com/luisa/alacena/AlacenaApplication.java
@@ -0,0 +1,13 @@
+package com.luisa.alacena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlacenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlacenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisa/alacena/AlacenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisa.alacena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlacenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
