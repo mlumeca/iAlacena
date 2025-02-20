077c4f3e32524fe608a1e8972ea7fb14ca3c45f6
luisa <meron.camar24@triana.salesianos.edu>
2025-02-20 13:25:29 +0100

    dockerfile done, falta actualizar docker compose

--- src/test/java/com/luisa/iAlacena/IAlacenaApplicationTests.java
+++ src/test/java/com/luisa/iAlacena/IAlacenaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.luisa.alacena;
+package com.luisa.iAlacena;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AlacenaApplicationTests {
+class IAlacenaApplicationTests {
 
 	@Test
 	void contextLoads() {
